a8d36ae2178b2e99724e257961f92b2f53ac24bd
bianca-ir <bianca.iii99@gmail.com>
2024-01-20 19:35:27 +0200

    Add models for training task

---
+++ SVM_train.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import SVC
+
+def SVM_train(df): 
+    train_df, test_df = train_test_split(df, test_size=0.2, random_state=42)
+
+    # Converting into numerical data using TF-IDF technique 
+    tfidf_vectorizer = TfidfVectorizer(max_features=5000) 
+
+    X_train_tfidf = tfidf_vectorizer.fit_transform(train_df['Text'])
+
+    # Extracting labels for training and testing
+    y_train = train_df['Label']
+    
+ 
+    svm_model = SVC(kernel='linear')
+    svm_model.fit(X_train_tfidf, y_train)
+
+    return svm_model 
+
+    
+
